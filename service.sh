b4fff8e76e1a8da4f0ecbab0e7f2fe25d2ffc224
yuqiqueens <yuqi.queens@gmail.com>
2017-04-12 17:15:42 -0400

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
-# Swiftstack service
 
 . /utils.sh
 
-#if [ -n "$gitTag" ]; then
-#    agentSendLogMessage  "Found gitTag parameter gitTag = ${gitTag}"
-#else
-#     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag=master"
-#     gitTag="swiftstack container create"
-#fi
+env
+print_log "$(env)"
+
+cmd=$1
+serviceStatus=""
+
+print_log "Beginning Service Script"
+
 
 sudo yum install -y python-pip
 sudo pip install pip --upgrade
 sudo pip install python-swiftclient
 
-cmd = $1
+print_log "Downloading ss01.py"
 
 # Get the python code for the service
-wget https://raw.githubusercontent.com/yuqiqueens/cc-ss01/master/ss01.py
+wget -N https://raw.githubusercontent.com/yuqiqueens/cc-ss01/master/ss01.py
 
-# Run the Service
-python ss01.py ${cmd}
+case $cmd in
+    update)
+		;;
+	start)
+		print_log "Beginning Start"
+		print_log "Executing ss01.py"
+		python ss01.py start
+		print_log "Start Complete"
+		sleep 6000
+		;;
+	stop)
+		;;
+	suspend)
+		;;
+	resume)
+		;;
+	*)
+		serviceStatus="No Valid Script Argument"
+		exit 127
+		;;
+esac
+print_log "Ending Service Script"
