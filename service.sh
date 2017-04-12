6f05ce82bc071da73d9ce63c7a74ca23a597275e
yuqiqueens <yuqi.queens@gmail.com>
2017-04-12 17:17:12 -0400

    Update service.sh

--- service.sh
+++ service.sh
@@ -22,23 +22,23 @@ wget -N https://raw.githubusercontent.com/yuqiqueens/cc-ss01/master/ss01.py
 
 case $cmd in
     update)
-		;;
-	start)
-		print_log "Beginning Start"
-		print_log "Executing ss01.py"
-		python ss01.py start
-		print_log "Start Complete"
-		sleep 6000
-		;;
-	stop)
-		;;
-	suspend)
-		;;
-	resume)
-		;;
-	*)
-		serviceStatus="No Valid Script Argument"
-		exit 127
-		;;
+	;;
+    start)
+	print_log "Beginning Start"
+	print_log "Executing ss01.py"
+	python ss01.py start
+	print_log "Start Complete"
+	sleep 6000
+	;;
+    stop)
+	;;
+    suspend)
+	;;
+    resume)
+	;;
+    *)
+	serviceStatus="No Valid Script Argument"
+	exit 127
+	;;
 esac
 print_log "Ending Service Script"
